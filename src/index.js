999af81773472228da181f57c1160750ee2936c5
1verapoles <47753672+1verapoles@users.noreply.github.com>
2019-10-07 10:55:36 +0200

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var stack = [];
+ var openm = [];
+var closem = [];
+var same = [];
+var closeIndex;
+var openIndex;
+var openIndex3;
+for (i=0; i<bracketsConfig.length; i++) {
+	openm[i] = bracketsConfig[i][0];
+}
+
+for (i=0; i<bracketsConfig.length; i++) {
+	closem[i] = bracketsConfig[i][1];
+}
+
+for (i=0; i<openm.length; i++) {
+	if (openm[i] === closem[i]) {
+		same.push(openm[i]);
+	}
+}
+
+for (i = 0; i < str.length; i++) {
+       openIndex = openm.indexOf(str[i]);
+
+       if (openIndex !== -1 && same.indexOf(str[i]) === -1) {
+           stack.push(openIndex);
+           console.log(stack);
+           continue;
+       }
+
+      if (openIndex !== -1 && same.indexOf(str[i])!== -1 && stack.indexOf(openIndex)!== -1) {
+           var z = stack.indexOf(openIndex);
+           console.log(z);
+          /* delete stack[z];*/
+          stack.splice(z,1);
+           console.log(stack);
+           continue;
+           }
+
+if (openIndex !== -1 && same.indexOf(str[i])!== -1 && stack.indexOf(openIndex)=== -1) {
+   stack.push(openIndex);
+           continue;
+       }
+       
+             
+  closeIndex = closem.indexOf(str[i]);
+
+       if (closeIndex !== -1 && same.indexOf(str[i]) === -1) {
+           openIndex3 = stack.pop();
+           if (closeIndex !== openIndex3) {
+               return false;
+           }
+          }
+
+if (closeIndex !== -1 && same.indexOf(str[i])!== -1 && stack.indexOf(closeIndex)!== -1) {
+           var z = stack.indexOf(closeIndex);
+           console.log(z);
+           /*delete stack[z];*/
+           stack.splice(z,1);
+                     }
+                      if (closeIndex !== -1 && same.indexOf(str[i])!== -1 && stack.indexOf(closeIndex)=== -1) {
+           openIndex3 = stack.pop();
+           if (closeIndex !== openIndex3) {
+               return false;
+           }
+          }
+
+ console.log(openm);
+    console.log(closem);
+    console.log(same);
+    console.log(stack);
+    }
+  if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
